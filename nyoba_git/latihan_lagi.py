f510d0b82647c11489fc65b9e8173f8de50e12a7
HafizdAmmar <hafizdammar@gmail.com>
2024-11-30 10:47:29 +0700

    Commit 1

---
+++ nyoba_git/latihan_lagi.py
@@ -0,0 +1,44 @@
+data_panen = {
+    'lokasi1':{
+        'nama lokasi':'Kebun A',
+        'hasil panen':{
+            'padi':1200,
+            'jagung':800,
+            'kedelai':500
+        }
+    },
+    'lokasi2':{
+        'nama lokasi':'Kebun B',
+        'hasil panen':{
+            'padi':1500,
+            'jagung':900,
+            'kedelai':450
+        }
+    },
+    'lokasi3':{
+        'nama lokasi':'Kebun C',
+        'hasil panen':{
+            'padi':1100,
+            'jagung':750,
+            'kedelai':600
+        }
+    },
+    'lokasi4':{
+        'nama lokasi':'Kebun D',
+        'hasil panen':{
+            'padi':1300,
+            'jagung':850,
+            'kedelai':550
+        }
+    },
+    'lokasi5':{
+        'nama lokasi':'Kebun E',
+        'hasil panen':{
+            'padi':1400,
+            'jagung':950,
+            'kedelai':480
+        }
+    }
+}
+
+print(data_panen)
